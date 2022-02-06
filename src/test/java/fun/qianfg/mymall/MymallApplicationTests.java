8295d84972e059ff7f4a5d54a166af50b2704fc3
qianfg <287541326@qq.com>
2022-02-06 21:46:54 +0800

    Initial commit

---
+++ src/test/java/fun/qianfg/mymall/MymallApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.qianfg.mymall;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
